437343240894a901145efa79a431b2e435e0e332
Kate Biernat <kebiernat5@gmail.com>
2021-03-30 01:06:57 -0400

    init

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE workPlace_DB;
+
+INSERT INTO department (name)
+VALUES ("Communications"),("Accounting")
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Macbook", "Tech", 100, 100),("Kombucha", "Beverage", 2, 2)
+
+INSERT INTO employee (item_name, category, starting_bid, highest_bid)
+VALUES ("Macbook", "Tech", 100, 100),("Kombucha", "Beverage", 2, 2)
